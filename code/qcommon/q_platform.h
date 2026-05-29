62a785e00b93348fada2e28f062ba128342ec8a7
Laurin Muth <l.muth@lyniat.games>
2026-05-29 11:34:56 +0200

    Add partial support for MSVC arm64

--- code/qcommon/q_platform.h
+++ code/qcommon/q_platform.h
@@ -79,7 +79,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #if defined(_WIN64) || defined(__WIN64__)
 
 #undef idx64
+#ifndef _M_ARM64
 #define idx64 1
+#endif
 
 #undef QDECL
 #define QDECL __cdecl
@@ -97,7 +99,14 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define PATH_SEP '\\'
 
 #if defined(__WIN64__) || defined(_WIN64)
+#if defined(_M_ARM64)
+#define ARCH_STRING "arm64"
+#ifndef NO_VM_COMPILED
+#define NO_VM_COMPILED
+#endif
+#else
 #define ARCH_STRING "x86_64"
+#endif
 #elif defined _M_ALPHA
 #define ARCH_STRING "AXP"
 #elif defined(__WIN32__) || defined(_WIN32)
